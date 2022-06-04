05a76f4a61300ce7aea8051710e65749d5791f12
Eduardo Finotti <eduardo.finotti@hotmail.com>
2022-06-04 10:30:23 -0300

    Adicionando step para arquivar relatório

--- src/test/java/runner/RunCucumber.java
+++ src/test/java/runner/RunCucumber.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/reports/cucumberTests.json", "html:target/reports/"},
         features = "src/test/resources/features",
         glue = {"steps"},
-        tags = {"@login-sucesso"}
+        tags = {"~@Ignore"}
 )
 public class RunCucumber extends RunBase{
 
